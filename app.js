f9dbc4457a05207caea3532fd96f4113a31c3776
Jhonata Silva <jhoncsilva28v@gmail.com>
2026-05-23 17:33:16 -0300

    atualização no app.js e index.html

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-let tituloJogo = document.querySelector('.titulo_do_jogo');
+let tituloJogo = document.querySelector('.titulo_do_jogo'); 
+// document - referencia o documento HTML
+// querySelector - busca alguma coisa no HTML
 tituloJogo.innerHTML = 'Jogo do número secreto';
+// innerHTML - adiciona algo ao HTML
 
 let paragrafo = document.querySelector('.texto__paragrafo');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 function verificarChute() {
+	// verificarChute - exibe mensagem quando o botão no HTML for clicado
 	console.log('Deu bom');
 }
